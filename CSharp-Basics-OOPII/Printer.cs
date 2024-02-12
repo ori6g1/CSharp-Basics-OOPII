ecc6e7fb58c7d68cfdfe4ee9e0263b1668059d58
Fortox <ori6g1@gmail.com>
2024-02-12 20:10:18 +0200

    Added A tasks
    oops forgot to fork

---
+++ CSharp-Basics-OOPII/Printer.cs
@@ -0,0 +1,59 @@
+﻿// ---- C# I (Dor Ben Dor) ----
+// Ori Shacham-Barr
+// ----------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// A - Method Overloading (W.I.C)
+
+enum OverflowBehavior
+{
+    Clip,
+    Overflow,
+    Ellipsis,
+}
+
+internal class Printer
+{
+    public string Text;
+    public int MaxLength;
+
+    public Printer(string text, int maxLength)
+    {
+        Text = text;
+        MaxLength = maxLength;
+    }
+
+    public void Print()
+    {
+        Print(OverflowBehavior.Clip);
+    }
+
+    public void Print(OverflowBehavior overflowBehavior)
+    {
+        switch (overflowBehavior)
+        {
+            case OverflowBehavior.Clip:
+                for (int i = 0; i < MaxLength && i < Text.Length; i++)
+                {
+                    Console.Write(Text[i]);
+                }
+                Console.WriteLine();
+                break;
+            case OverflowBehavior.Overflow:
+                Console.WriteLine(Text);
+                break;
+            case OverflowBehavior.Ellipsis:
+                for (int i = 0; i < MaxLength && i < Text.Length; i++)
+                {
+                    Console.Write(Text[i]);
+                }
+                Console.WriteLine("...");
+                break;
+        }
+    }
+}
--- CSharp-Basics-OOPII/Program.cs
+++ CSharp-Basics-OOPII/Program.cs
@@ -1,3 +1,12 @@
 ﻿// ---- C# I (Dor Ben Dor) ----
 // Ori Shacham-Barr
 // ----------------------------
+
+// A - Method Overloading (W.I.C)
+
+Printer p = new Printer("ABCDEFG", 3);
+
+p.Print();
+p.Print(OverflowBehavior.Overflow);
+p.Print(OverflowBehavior.Clip);
+p.Print(OverflowBehavior.Ellipsis);
