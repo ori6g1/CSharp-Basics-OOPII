dac0989ccd3ecdc1bb28e0a56cbfe0ca406679fd
Fortox <ori6g1@gmail.com>
2024-03-03 16:25:17 +0200

    Added task B

--- CSharp-Basics-OOPII/Program.cs
+++ CSharp-Basics-OOPII/Program.cs
@@ -2,11 +2,36 @@
 // Ori Shacham-Barr
 // ----------------------------
 
-// A - Method Overloading (W.I.C)
+using System.Numerics;
 
-Printer p = new Printer("ABCDEFG", 3);
+class Program
+{
+    static void Main(String[] args)
+    {
+        string LineBreak = "\r\n";
 
-p.Print();
-p.Print(OverflowBehavior.Overflow);
-p.Print(OverflowBehavior.Clip);
-p.Print(OverflowBehavior.Ellipsis);
+        // A - Method Overloading (W.I.C)
+        Console.WriteLine("A - Method Overloading (W.I.C)");
+        Printer p = new Printer("ABCDEFG", 3);
+
+        p.Print();
+        p.Print(OverflowBehavior.Overflow);
+        p.Print(OverflowBehavior.Clip);
+        p.Print(OverflowBehavior.Ellipsis);
+        Console.WriteLine(LineBreak);
+
+        // B - Properties (W.I.C)
+        Rectangle r1 = new Rectangle(4f, 4f, Vector2.Zero);
+        Rectangle r2 = new Rectangle(4f, 4f, new Vector2(4.1f, 4f));
+
+        if (r1.IsPointInRectangle(new Vector2(1f, 2.1f)))
+            Console.WriteLine("The point is in the rectangle.");
+        else
+            Console.WriteLine("The point is not in the rectangle.");
+
+        if (r1.DoRectanglesIntersect(r2))
+            Console.WriteLine("The rectangles intersect.");
+        else
+            Console.WriteLine("The rectangles don't intersect.");
+    }
+}
---
+++ CSharp-Basics-OOPII/Rectangle.cs
@@ -0,0 +1,72 @@
+﻿// ---- C# I (Dor Ben Dor) ----
+// Ori Shacham-Barr
+// ----------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+// B - Properties (W.I.C)
+enum Vertex
+{
+    TopLeft,
+    TopRight, 
+    BottomLeft, 
+    BottomRight,
+}
+
+internal class Rectangle
+{
+    public float Width { get; private set; }
+    public float Height { get; private set; }
+    public Vector2 Center { get; private set; }
+    public Vector2[] Vertexes { get; private set; }
+
+    public float Volume { get {  return Width * Height; } }
+    public float Circumference { get {  return (Height + Width) * 2; } }
+
+    public Rectangle(float width, float height, Vector2 center)
+    {
+        Width = width;
+        Height = height;
+        Center = center;
+
+        Vertexes = new Vector2[4];
+        for (int i = 0; i < Vertexes.Length; i++)
+        {
+            switch (i) 
+            {
+                case (int)Vertex.TopLeft:
+                    Vertexes[i] = new Vector2(Center.X - (Width / 2), Center.Y + (Height / 2));
+                    break;
+                case (int)Vertex.TopRight:
+                    Vertexes[i] = new Vector2(Center.X + (Width / 2), Center.Y + (Height / 2));
+                    break;
+                case (int)Vertex.BottomLeft:
+                    Vertexes[i] = new Vector2(Center.X - (Width / 2), Center.Y - (Height / 2));
+                    break;
+                case (int)Vertex.BottomRight:
+                    Vertexes[i] = new Vector2(Center.X + (Width / 2), Center.Y - (Height / 2));
+                    break;
+            }
+        }
+    }
+
+    public bool IsPointInRectangle(Vector2 point)
+    {
+        if (point.X >= Vertexes[(int)Vertex.TopLeft].X && point.X <= Vertexes[(int)Vertex.BottomRight].X &&
+            point.Y <= Vertexes[(int)Vertex.TopLeft].Y && point.Y >= Vertexes[(int)Vertex.BottomRight].Y)
+            return true;
+        return false;
+    }
+
+    public bool DoRectanglesIntersect(Rectangle rectangle)
+    {
+        for (int i = 0; i < Vertexes.Length; i++)
+            if (IsPointInRectangle(rectangle.Vertexes[i]))
+                return true;
+        return false;
+    }
+}
